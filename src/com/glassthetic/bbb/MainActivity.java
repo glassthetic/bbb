eb3701061fc9df232781ee8c0f69737912c1a0c4
Paul Young <paulyoungonline@gmail.com>
2013-12-12 22:06:34 -0500

    Updated to use API init method.

--- src/com/glassthetic/bbb/MainActivity.java
+++ src/com/glassthetic/bbb/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		DribbbleAPI.setContext(this);		
+		DribbbleAPI.init(this);
 	}
 
 	@Override
